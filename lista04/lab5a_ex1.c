798e9084c8a8d4350c8306ee13a15c4c5b49dc1e
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 21:38:52 -0300

    Update lab5a_ex1.c

--- lista04/lab5a_ex1.c
+++ lista04/lab5a_ex1.c
@@ -1,4 +1,25 @@
-/* Gian Lucca - 10438361 */
+/*
+Escreva uma função em linguagem C chamada fatorial que receba 
+um valor inteiro 𝑛 ≥ 0 digitado pelo usuário e devolva o fatorial de 𝑛. Usamos a 
+notação 𝑛! para denotar o fatorial de 𝑛. Ou seja, 𝑛! = 𝑛 ∗ (𝑛 − 1) ∗ (𝑛 − 2) ∗ … ∗
+2 ∗ 1
+Algumas observações: 
+1) A função acima deve ser iterativa 
+2) Após a execução da função fatorial, deve-se perguntar ao usuário do 
+programa se ele deseja continuar ou não. Se o usuário desejar continuar 
+então, neste caso, o programa deve solicitar que o usuário digite um novo 
+valor inteiro para o cálculo do fatorial. 
+3) O programa deve realizar a validação dos dados de entrada numéricos. Por 
+exemplo. O programa não deve aceitar valores de entrada (digitados pelo 
+usuário) negativos. Neste caso, deve-se exibir uma mensagem de aviso ao 
+usuário e permitir que ele tente novamente. O usuário possui 3 tentativas 
+consecutivas para entrar com um dado válido. Se após 3 tentativas 
+consecutivas o usuário não tenha digitado um dado de entrada válido (ou 
+seja, um número ≥ 0), então neste caso o programa deve exibir a mensagem 
+ao usuário “Número de tentativas excedeu o limite! Tente mais tarde” e 
+encerrar o programa. 
+4) Você deve incluir também em seu programa o protótipo da função fatorial
+*/
 
 #include <stdio.h>
 
