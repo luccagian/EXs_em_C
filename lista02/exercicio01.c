cafa6d7a1791db511ba7c4df82c0bd5cbe7e0c99
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 21:29:34 -0300

    Update exercicio01.c

--- lista02/exercicio01.c
+++ lista02/exercicio01.c
@@ -1,3 +1,11 @@
+/*
+Crie um programa que imprime na tela os seguintes textos. Tente não criar variáveis, ou seja, use apenas 
+a função printf. 
+a) Vênus está a 67 milhões de milhas do sol. A letra J pronuncia-se Jota.
+b) O primeiro número é 44
+c) O segundo número é ‘num1 + 20.5’ = 64.5
+*/
+
 #include <stdio.h>
 
 int main()
